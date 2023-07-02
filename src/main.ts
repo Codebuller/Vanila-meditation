cc81f1702df633e5526c99922dda5349036272f0
My Name <ivan_1337_qwerty_339@mail.ru>
2023-07-02 19:34:34 +0300

    Stupid ts

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,14 @@ import './style.css'
 import { setupCounter } from './counter.ts'
 
 const app = () =>{
-  const song: HTMLAudioElement|null = document.querySelector('.song');
-const play: HTMLImageElement|null = document.querySelector('.play');
-const outline: SVGCircleElement|null = document.querySelector('.moving-outline circle');
-const video: HTMLVideoElement|null = document.querySelector('.vid-container video');
+  const song: any = document.querySelector('.song');
+const play: any = document.querySelector('.play');
+const outline: any = document.querySelector('.moving-outline circle');
+const video: any = document.querySelector('.vid-container video');
 const pickTimeButtons: NodeListOf<HTMLButtonElement> = document.querySelectorAll('.time-selector button');
 const sounds: NodeListOf<HTMLButtonElement> = document.querySelectorAll('.sound-picker button');
 
-  const timeDisplay = document.querySelector('.time-display')
+  const timeDisplay:any = document.querySelector('.time-display')
   
   const outlineLength = outline?.getTotalLength();
   
@@ -24,7 +24,7 @@ const sounds: NodeListOf<HTMLButtonElement> = document.querySelectorAll('.sound-
   play.addEventListener('click',() =>{
     check(song);
   })
-  pickTimeButtons.forEach((e)=>{e.addEventListener('click',(e) =>{
+  pickTimeButtons.forEach((e)=>{e.addEventListener('click',(e:any) =>{
     duration = e.srcElement.attributes[0].value;
     reset();
     currentTime = song.currentTime;
@@ -52,7 +52,7 @@ song.ontimeupdate = () =>{
   }
 }
 sounds.forEach((elem)=>{
-  elem.addEventListener('click',(e)=>{
+  elem.addEventListener('click',()=>{
    
     song.src = elem.getAttribute('data-sound');
     video.src = elem.getAttribute('data-video');
@@ -62,7 +62,7 @@ sounds.forEach((elem)=>{
    
   })
 })
-const check = song =>{
+const check = (song:any) =>{
   if(song.paused){
     play.src = '../public/svg/pause.svg';
     video.play();
